5310abd9a2e6bf9be27f8cf57ef80462e795f506
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2024-04-18 12:46:40 -0700

    feat: do not require `layouts` input

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ try {
   // Input defined in action metadata file
   const EXTNAME = getInput('extname')
   const ROOT = getInput('root')
-  const LAYOUTS = getInput('layouts')
   const PARTIALS = getInput('partials')
+  const LAYOUTS = getInput('layouts') || undefined
   const FILE = getInput('file')
 
   // Liquidjs engine
